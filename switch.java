c7c1764ed5623c069190a6bebc40bf97a54ec44c
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2022-07-19 21:18:44 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+String str=sc.next();
+switch (str){
+    case "Mango":
+        System.out.println("King of Fruits");
+        break;
+    case "Apple":
+        System.out.println("Red fruit");
+    
+    case "Grapes":
+        System.out.println("green fruit");
+ 
+
+  default :
+        System.out.println("please enter a valid fruit ");
+        
+
+        
+        
+}
+}
+}
