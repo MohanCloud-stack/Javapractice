f0f28b5289cf171c3f1d5afc12a0af91f6481d24
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2022-07-22 19:57:16 +0530

    creating shadowing java
    creating shadowing java

---
+++ Shadowing.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Main {
+
+public static void main(String[] args) {
+      fun(2,3,4,5,6,3,4,4);
+    }
+static void fun(int ...v){
+    System.out.println(Arrays.toString(v));
+}
+}
+       
+          
+    
+
