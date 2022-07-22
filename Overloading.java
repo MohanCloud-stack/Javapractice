b2cd84ba75318f32a96b6127c59b90d47c296357
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2022-07-22 20:09:15 +0530

    Create Overloading.java

---
+++ Overloading.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Main {
+static void fun(int a){
+ System.out.println(a);
+}
+static void  fun(String str){
+    System.out.println(str);
+}
+    
+public static void main(String[] args) {
+    fun(23);
+    fun("mohan");
+}
+}
+       
+          
+    
+
